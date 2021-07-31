82647bfdde15211e4f6b0eea5b37fcdd1bf72808
kolteshubham517 <johndoe@example.com>
2021-07-31 21:17:15 +0530

    add statics and method, refactor signup route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
+
+// microservice routes
+import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
+
+// Database connection
+import ConnectDB from "./database/connection";
 
 const zomato = express();
 
@@ -9,7 +25,23 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
-zomato.get("/", (req, res) => res.json({ message: "setup success..."}));
+// passport cofiguration
+googleAuthConfig(passport);
 
-zomato.listen(4000, () => console.log("Server is running 🚀"));
+// Application Routes
+zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/food", Food);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
